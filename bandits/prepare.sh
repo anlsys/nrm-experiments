80853cbb0d1a0b29784c4da1f0e6860a7db5604d
Valentin Reis <vreis@anl.gov>
2020-08-25 10:45:47 -0500

    updates default values

--- bandits/prepare.sh
+++ bandits/prepare.sh
@@ -7,7 +7,8 @@ sudo chown -R "$(whoami)":users /sys/devices/virtual/powercap/intel-rapl
 #echo "280000000" > /sys/devices/virtual/powercap/intel-rapl/intel-rapl:1/constraint_0_power_limit_uw
 #echo "280000000" > /sys/devices/virtual/powercap/intel-rapl/intel-rapl:0/constraint_0_power_limit_uw
 
-echo "999999999999" > /sys/devices/virtual/powercap/intel-rapl/intel-rapl:0/constraint_0_time_window_us
-echo "280000000"    > /sys/devices/virtual/powercap/intel-rapl/intel-rapl:0/constraint_0_power_limit_uw
-echo "999999999999" > /sys/devices/virtual/powercap/intel-rapl/intel-rapl:1/constraint_0_time_window_us
-echo "280000000"    > /sys/devices/virtual/powercap/intel-rapl/intel-rapl:1/constraint_0_power_limit_uw
+echo "999424"       > /sys/devices/virtual/powercap/intel-rapl/intel-rapl:0/constraint_0_time_window_us
+echo "125000000"    > /sys/devices/virtual/powercap/intel-rapl/intel-rapl:0/constraint_0_power_limit_uw
+
+echo "999424"       > /sys/devices/virtual/powercap/intel-rapl/intel-rapl:1/constraint_0_time_window_us
+echo "125000000"    > /sys/devices/virtual/powercap/intel-rapl/intel-rapl:1/constraint_0_power_limit_uw
