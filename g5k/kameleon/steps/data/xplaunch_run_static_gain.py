fc761b4179fbf4bcbb8d5c5b0cbbac81f4c7541b
Raphaël Bleuse <raphael.bleuse@inria.fr>
2020-11-05 18:04:24 +0100

    Integrate modifications induced by  pynrm refactor

--- g5k/kameleon/steps/data/xplaunch_run_static_gain.py
+++ g5k/kameleon/steps/data/xplaunch_run_static_gain.py
@@ -14,42 +14,37 @@ def noop(*args, **kwargs):
     pass
 
 
-def enforce_powercap(host, powercap):
+def enforce_powercap(daemon, powercap):
     # this relies only on the configuration of the daemon, so we do not need to
     # wait for the application to start
-    cpd = dict(host.get_cpd())
-    import pprint; pprint.pprint(cpd, stream=sys.stderr)  # XXX: logs
+    cpd = daemon.get_cpd()
+    import pprint; pprint.pprint(vars(cpd), stream=sys.stderr)  # XXX: logs
 
     # get all RAPL actuator (XXX: should filter on tag rather than name)
-    rapl_actuators = filter(lambda a: a[0].startswith('RaplKey'), cpd['actuators'])
+    import pprint; pprint.pprint(cpd.actuators())
+    rapl_actuators = filter(lambda a: a.actuatorID.startswith('RaplKey'), cpd.actuators())
 
     # for each RAPL actuator, create an action that sets the powercap to powercap
     set_pcap_actions = [nrm.Action(actuator[0], powercap) for actuator in rapl_actuators]
     import pprint; pprint.pprint(set_pcap_actions, stream=sys.stderr)  # XXX: logs
 
-    host.workload_action(set_pcap_actions)
+    daemon.actuate(set_pcap_actions)
 
 
 def launch_application(daemon_cfg, workload_cfg, *, setup=noop, teardown=noop, sleep_duration=0.5):
-    try:
-        # launch daemon
-        host = nrm.Local()
-        host.start_daemon(daemon_cfg)
-        host.check_daemon()
+    with nrm.nrmd(daemon_cfg) as daemon:
+        # extra daemon configuration before starting workload
+        setup(daemon)
 
-        setup(host)
-
-        # launch and wait for the end
-        host.run_workload(workload_cfg)
-        while host.check_daemon() and not host.workload_finished():
+        # launch workload, and wait for the end of the execution
+        daemon.run(**workload_cfg)
+        while not daemon.all_finished():
+            # msg = daemon.upstream_recv()
             time.sleep(sleep_duration)
 
-        teardown(host)
 
-    finally:
-        # stop daemon
-        host.stop_daemon()
-        assert not host.check_daemon()
+        # extra work once the workload execution is over
+        teardown(daemon)
 
 
 # main script  ################################################################
@@ -83,35 +78,31 @@ def run(options, cmd):
     daemon_cfg = {
         'raplCfg': {
             'raplActions': [
-                {'fromuW': 1_000_000 * options.powercap},
+                {'microwatts': 1_000_000 * options.powercap},
             ],
-            'raplFrequency': {'fromHz': 1},
-            'raplPath': '/sys/devices/virtual/powercap/intel-rapl',
         },
         'verbose': 'Debug',
         # 'verbose': 'Info',
     }
 
     # workload configuration (i.e., app description + manifest)
-    workload_cfg = [
-        {
-            'cmd': cmd[0],
-            'args': cmd[1:],
-            'sliceID': 'sliceID',  # XXX: bug in pynrm/hnrm if missing or None (should be generated?)
-            'manifest': {  # XXX: pynrm/hnrm crashes if missing
-                # 'app': {
-                #     'slice': {
-                #         'cpus': 1,
-                #         'mems': 1,
-                #     },
-                #     'perfwrapper': {
-                #         'perfLimit': {'fromOps': 100_000},
-                #         'perfFreq': {'fromHz': 1},
-                #     },
-                # },
-            },
+    workload_cfg = {
+        'cmd': cmd[0],
+        'args': cmd[1:],
+        'sliceID': 'sliceID',  # XXX: bug in pynrm/hnrm if missing or None (should be generated?)
+        'manifest': {  # XXX: pynrm/hnrm crashes if missing
+            # 'app': {
+            #     'slice': {
+            #         'cpus': 1,
+            #         'mems': 1,
+            #     },
+            #     'perfwrapper': {
+            #         'perfLimit': {'fromOps': 100_000},
+            #         'perfFreq': {'fromHz': 1},
+            #     },
+            # },
         },
-    ]
+    }
     # NB: if we want to keep the output of the launched command when run in
     # detached mode (which is the case with the Python API), we can wrap the call
     # in a shell
@@ -123,7 +114,7 @@ def run(options, cmd):
     launch_application(
         daemon_cfg,
         workload_cfg,
-        setup=lambda host: enforce_powercap(host, options.powercap),
+        setup=lambda daemon: enforce_powercap(daemon, options.powercap),
     )
 
 
