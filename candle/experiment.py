8e1e2d1787c6d2d1ed72f78af254b4d4ffb5ae92
Chameleon Cloud User <cc@aoeu.novalocal>
2021-02-23 22:05:03 +0000

    experiment file with example actuator pull

--- candle/experiment.py
+++ candle/experiment.py
@@ -6,12 +6,40 @@ import sys
 import time
 import nrm.tooling as nrm
 
-manifest = {}
+actuatorID = "candleActuator"
+
+manifest = {
+    "app": {
+        "actuators": [
+            {
+                "actuatorID": actuatorID,
+                "actuator": {
+                    "actuatorBinary": "bash",
+                    "actuatorArguments": ["-c", "kill -SIGUSR1 $(cat ./pid)"],
+                    "actions": [1],
+                    "referenceAction": 1,
+                },
+            }
+        ]
+    }
+}
+
 daemonCfg = {
     # "verbose": "Info",
     "raplCfg": None
 }
 
+#
+##################         ##############            ########                       #######
+# candle (nt3_*) # <-exec- # wrapper.sh # <-sigUSR1- # nrmd # <--run with manifest- # nrm #
+##################         ##############            ########                       #######
+#                                                        ^ daemon.actuate()
+#                                                        | # upstream API
+#                                                        v daemon.upstream_recv()
+#                                                    #################
+#                                                    # experiment.py # <- the smart one
+#                                                    #################
+#
 
 def do_workload(daemonCfg, manifest):
     with nrm.nrmd(daemonCfg) as d:
@@ -20,8 +48,12 @@ def do_workload(daemonCfg, manifest):
         )
         while not d.all_finished():
             msg = d.upstream_recv()
+            print("SLEEP")
+            time.sleep(10)
+            print("ACTUATE")
+            d.actuate(Action(actuatorID, 1))
+            print("ACTUATED")
             print(msg)
-
 
 if __name__ == "__main__":
     do_workload(daemonCfg, manifest)
