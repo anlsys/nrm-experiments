d056865b815f7cb3b9d93b4e23e0849e814ce1a1
Idriss Daoudi <hatoshi@hatoshis-MacBook-Pro.local>
2021-10-25 16:27:58 +0200

    NRM topology tool - fixes

--- nrm-topology/headers/cpu.h
+++ nrm-topology/headers/cpu.h
@@ -40,13 +40,13 @@ int get_cpus (struct device** devices, hwloc_topology_t topology, hwloc_obj_t ob
 				const char* backend = hwloc_obj_get_info_by_name(object, "Backend");
 
 				strcpy(devices[index]->type, type);
-				strlen(model) > 0 ? strcpy(devices[index]->name, model) : strcpy(devices[index]->name, "None");
+				model != NULL ? strcpy(devices[index]->name, model) : strcpy(devices[index]->name, "None");
 				backend != NULL ? strcpy(devices[index]->backend, backend) : strcpy(devices[index]->backend, "None");
 				devices[index]->resource_id = object->gp_index;
 
 				int memory_counter = 0;
 				int compute_counter = 0;
-				char cousins_to_string[STR_SIZE], size_to_string[STR_SIZE];
+				char compute_units_to_string[STR_SIZE], size_to_string[STR_SIZE];
 
 				int arity = object->arity;
 				for (int k = 0; k < arity; k++)
@@ -58,13 +58,12 @@ int get_cpus (struct device** devices, hwloc_topology_t topology, hwloc_obj_t ob
 					hwloc_obj_t current_object = object->children[k];
 					hwloc_obj_type_snprintf(type_current_object, sizeof(type_current_object), current_object, 0);
 
-					int my_arity = current_object->arity;
 					if (hwloc_obj_type_is_cache(current_object->type))
 					{
 						strcpy(devices[index]->memory[memory_counter], type_current_object);
-						int cousins = hwloc_get_nbobjs_by_type(topology, current_object->type);
-						sprintf(cousins_to_string, "%d", cousins/nb_of_packages);
-						strcpy(devices[index]->memory[memory_counter + 1], cousins_to_string);
+						int compute_units = hwloc_get_nbobjs_by_type(topology, current_object->type);
+						sprintf(compute_units_to_string, "%d", compute_units/nb_of_packages);
+						strcpy(devices[index]->memory[memory_counter + 1], compute_units_to_string);
 						size = ((double)current_object->attr->cache.size / 1024);
 						sprintf(size_to_string, "%llu", size);
 						strcpy(devices[index]->memory[memory_counter + 2], size_to_string);
@@ -74,9 +73,9 @@ int get_cpus (struct device** devices, hwloc_topology_t topology, hwloc_obj_t ob
 					else
 					{
 						strcpy(devices[index]->compute[compute_counter], type_current_object);
-						int cousins = hwloc_get_nbobjs_by_type(topology, current_object->type);
-						sprintf(cousins_to_string, "%d", cousins/nb_of_packages);
-						strcpy(devices[index]->compute[compute_counter + 1], cousins_to_string);
+						int compute_units = hwloc_get_nbobjs_by_type(topology, current_object->type);
+						sprintf(compute_units_to_string, "%d", compute_units/nb_of_packages);
+						strcpy(devices[index]->compute[compute_counter + 1], compute_units_to_string);
 						
 						compute_counter += 2;	
 					}
