7ad0bde2a5ca78c13ae6b5c7f5599fd84715ddfd
datoshi <idaoudi@anl.gov>
2021-11-15 15:18:56 +0100

    fixes

--- mpi/primes.c
+++ mpi/primes.c
@@ -15,40 +15,48 @@
  *
  * =====================================================================================
  */
+
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <inttypes.h>
 #include <time.h>
+#include <sched.h>
 #include "mpi.h"
 
 #define LIMIT     36
 #define FIRST     0
 
-void time_stamp(int rank, int n, FILE* file)
+void time_stamp(int rank, int n)
 {
-    long ms; // Milliseconds
-    time_t s;  // Seconds
-    struct timespec spec;
+	long ms; // Milliseconds
+	time_t s;  // Seconds
+	struct timespec spec;
 
-    clock_gettime(CLOCK_REALTIME, &spec);
+	clock_gettime(CLOCK_REALTIME, &spec);
 
-    s  = spec.tv_sec;
-    ms = round(spec.tv_nsec / 1.0e6); // Convert nanoseconds to milliseconds
-    if (ms > 999) {
-        s++;
-        ms = 0;
-    }
+	s  = spec.tv_sec;
+	ms = round(spec.tv_nsec / 1.0e6); // Convert nanoseconds to milliseconds
+	if (ms > 999) {
+		s++;
+		ms = 0;
+	}
 	char name[128];
-	int size;
-	//MPI_Get_processor_name(name, &size);
+	int size, cpu;
 
+	cpu = sched_getcpu();
+	printf("************************\n");
+	printf("Time: %"PRIdMAX".%03ld\n", (intmax_t)s, ms);
+	printf("Rank: %d\n", rank);
+	printf("Checked: %d\n", n);
+	printf("Scope: %d\n", cpu);
 	
-    fprintf(file, "Time: %"PRIdMAX".%03ld\n", (intmax_t)s, ms);
-	fprintf(file, "Rank: %d\n", rank);
-	fprintf(file, "Checked: %d\n", n);
-	//fprintf(file, "Scope: %s\n", name);
-	fprintf(file, "\n");
+	//fprintf(file, "Time: %"PRIdMAX".%03ld\n", (intmax_t)s, ms);
+	//fprintf(file, "Rank: %d\n", rank);
+	//fprintf(file, "Checked: %d\n", n);
+	//fprintf(file, "Scope: %d\n", cpu);
+	//fprintf(file, "\n");
 
 }
 
@@ -74,21 +82,21 @@ int isprime(int n) {
 int main ()
 {
 	int   nranks,
-		  rank,
-		  n,
-		  prime_counter,
-		  pcsum,
-		  found,
-		  maxprime,
-		  mystart,
-		  jump;
+	      rank,
+	      n,
+	      prime_counter,
+	      pcsum,
+	      found,
+	      maxprime,
+	      mystart,
+	      jump;
 
 	double start_time, end_time;
 
 	MPI_Init(NULL, NULL);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &nranks);
-	
+
 	if (((nranks % 2) != 0) || ((LIMIT % nranks) != 0)) 
 	{
 		printf("Number of ranks must be even and must divide the LIMIT!\n");
@@ -102,9 +110,9 @@ int main ()
 	prime_counter=0;
 	found = 0;
 
-	FILE *file;
-	file = fopen("./log.rec", "w+");
-	
+	//FILE *file;
+	//file = fopen("./log.rec", "w+");
+
 	if (rank == FIRST)
 	{
 		// Jump the first 4 primes (<10)
@@ -115,9 +123,9 @@ int main ()
 			{
 				prime_counter++;
 				found = n;
-				printf("%d\n",found);
+				//printf("%d\n",found);
 			}
-			time_stamp(rank, n, file);
+			time_stamp(rank, n);
 		}
 		MPI_Reduce(&prime_counter, &pcsum, 1, MPI_INT, MPI_SUM, FIRST, MPI_COMM_WORLD);
 		MPI_Reduce(&found, &maxprime, 1, MPI_INT, MPI_MAX, FIRST, MPI_COMM_WORLD);
@@ -133,14 +141,14 @@ int main ()
 			{
 				prime_counter++;
 				found = n;
-				printf("%d\n",found);
+				//printf("%d\n",found);
 			}
-			time_stamp(rank, n, file);
+			time_stamp(rank, n);
 		}
 		MPI_Reduce(&prime_counter, &pcsum, 1, MPI_INT, MPI_SUM, FIRST, MPI_COMM_WORLD);
 		MPI_Reduce(&found, &maxprime, 1, MPI_INT, MPI_MAX, FIRST, MPI_COMM_WORLD);
 	}
 
-	fclose(file);
+	//fclose(file);
 	MPI_Finalize();
 }
